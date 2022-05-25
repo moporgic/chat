f5ac2faf1cdd3e160a14556091bbb1703da96f0e
Hung Guei <hguei@moporgic.info>
2022-05-26 02:39:35 +0800

    also print hostname

--- envinfo.sh
+++ envinfo.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # envinfo.sh: display the current environment
-# verions 2022-05-19 by moporgic
+# verions 2022-05-26 by moporgic
 
+# host name
+echo "Host: $(hostname)"
+# OS name and version
+osinfo=$(uname -o 2>/dev/null | sed "s|GNU/||")
+osinfo+=" $(uname -r | sed -E 's/[^0-9.]+.+$//g')"
+if [[ $OSTYPE =~ cygwin|msys ]]; then
+	ver=($(cmd /c ver 2>/dev/null | tr "[\r\n]" " "))
+	(( ${#ver[@]} )) && osinfo+=" (Windows ${ver[-1]})"
+fi
+echo "OS: $osinfo"
 # CPU model
 cpuinfo=$(grep -m1 name /proc/cpuinfo | sed -E 's/.+:|\(\S+\)|CPU|[0-9]+-Core.+|@.+//g' | xargs)
 nodes=$(lscpu | grep 'NUMA node(s)' | cut -d: -f2 | xargs)
@@ -21,11 +31,3 @@ nvidia-smi -L 2>/dev/null | sed -E "s/ \(UUID:.+$//g" | while IFS= read GPU; do 
 # memory info
 size=($(head -n1 /proc/meminfo))
 echo "RAM: $(printf "%.1fG" $(<<< "${size[1]}/1024/1024" bc -l))"
-# OS name and version
-osinfo=$(uname -o 2>/dev/null | sed "s|GNU/||")
-osinfo+=" $(uname -r | sed -E 's/[^0-9.]+.+$//g')"
-if [[ $OSTYPE =~ cygwin|msys ]]; then
-	ver=($(cmd /c ver 2>/dev/null | tr "[\r\n]" " "))
-	(( ${#ver[@]} )) && osinfo+=" (Windows ${ver[-1]})"
-fi
-echo "OS: $osinfo"
